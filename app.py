595a3f55ded96859a3bfe7e26995faeb51a4ece1
Simon Schneemelcher <schneemelchercom@gmail.com>
2022-12-24 17:24:50 +0100

    add multiline support

--- app.py
+++ app.py
@@ -16,21 +16,22 @@ def chat():
     chat_history = request.form['history']
     # print(chat_history)
     # Process the chat history and generate a response
-    response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=generate_prompt(chat_history),
-        temperature=0.6,
-        max_tokens=256,
-        stop="Student:"
-    )
-    return response['choices'][0].text
+    # response = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     prompt=generate_prompt(chat_history),
+    #     temperature=0.6,
+    #     max_tokens=256,
+    #     stop="Student:"
+    # )
+    # return response['choices'][0].text
+    return 'hello!'
 
 
 def generate_prompt(prompt):
     return """
 You are StudyBot, an AI assistant created to help students at solving their homework. 
-You are always trying your best helping students and guiding them through their tasks, but you will not do the homework for them, rather with them.
+You are always trying your best helping students and guiding them through their tasks,
+as well as providing encouragement. The following is a conversation between you and a student.
 
-This is a conversation between you and a student:
 Student: {}
 StudyBot:""".format(prompt)
